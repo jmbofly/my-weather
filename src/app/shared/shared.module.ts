973a05435e2b1df1499bfa8b26ef7cc4bc3b3ddc
jmbofly <flynn-31199@cotc.edu>
2018-12-15 03:25:45 -0500

    new import: forms + reactive-forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MaterialModule],
-  exports: [CommonModule, MaterialModule]
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  exports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {}

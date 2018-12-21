bb6f74ffe5b840eb879740e4a05e18e4451c08f2
jmbofly <flynn-31199@cotc.edu>
2018-12-21 02:20:10 -0500

    changed router configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app-routing.module';
+import { LocationModule } from './location/location.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FlexLayoutModule, BrowserAnimationsModule, SharedModule, CoreModule],
+  imports: [BrowserModule, FlexLayoutModule, BrowserAnimationsModule, SharedModule, CoreModule, HomeModule, AppRoutingModule, LocationModule],
   providers: [],
   bootstrap: [AppComponent]
 })

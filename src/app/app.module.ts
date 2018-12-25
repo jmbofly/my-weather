e478b705cef1bddf1e702cc72d1f36c9b0909ca7
jmbofly <flynn-31199@cotc.edu>
2018-12-25 06:51:22 -0500

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-
 import { CoreModule } from './core/core.module';
-
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule, CoreModule],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedModule, CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
